886a016f2ba374e9900eac34030f20b87435b650
Maxim Khan-Magomedov <maxim.km@gmail.com>
2024-11-01 00:43:47 +0300

    Add JWT-related methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,46 @@
 
 # Application controller
 class ApplicationController < ActionController::API
+  def encode_token(payload)
+    JWT.encode(payload, jwt_key)
+  end
+
+  def jwt_token
+    @jwt_token ||= decoded_token
+  end
+
+  def current_user
+    @current_user ||= user_from_token
+  end
+
+  def require_authentication
+    return unless current_user.nil?
+
+    render json: { message: 'Please log in' }, status: :unauthorized
+  end
+
+  private
+
+  def jwt_key
+    @jwt_key ||= ENV.fetch('JWT_KEY')
+  end
+
+  def decoded_token
+    header = request.headers['Authorization']
+    return unless header
+
+    token = header.split[1]
+    begin
+      JWT.decode(token, jwt_key)
+    rescue JWT::DecodeError
+      nil
+    end
+  end
+
+  def user_from_token
+    return unless jwt_token
+
+    user_id = decoded_token[0]['user_id']
+    User.find_by(id: user_id)
+  end
 end
